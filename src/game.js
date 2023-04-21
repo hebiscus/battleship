e5f378e3bdffd2694bf6c9057dde9549a50198ac
hebiscus <107350293+hebiscus@users.noreply.github.com>
2023-04-21 20:10:09 +0200

    board render, placing ships beginning

--- src/game.js
+++ src/game.js
@@ -7,24 +7,38 @@ import { playerFactory } from './player';
 import { renderBoard, renderPlacingShips } from '.';
 
 
-export function game() {
+export function GameController() {
     const human = playerFactory("human");
     const computer = playerFactory("computer");
-
+    const players = [human, computer]
     const humanBoard = gameBoardFactory();
-    humanBoard.createBoard();
     const computerBoard = gameBoardFactory();
-    computerBoard.createBoard();
 
-    renderBoard("human", humanBoard.currentBoard);
-    renderBoard("computer", computerBoard.currentBoard);
+    const boards = [humanBoard, computerBoard]
+    const activePlayer = players[0];
+    const shipsArray = [];
 
-    const ships = getShips();
+    const createShips = () => {
+        const patrolBoat = shipFactory(2,0, "patrolBoat");
+        const sumbarine = shipFactory(3,0, "submarine");
+        const destroyer = shipFactory(3,0, "destroyer");
+        const battleship = shipFactory(4,0, "battleship");
+        const carrier = shipFactory(5,0, "carrier");
+        shipsArray.push(patrolBoat, sumbarine, destroyer, battleship, carrier);
+        return shipsArray;
+    }
 
-    renderPlacingShips(humanBoard, ships)
+    const playRound = () => {
+        humanBoard.createBoard();
+        computerBoard.createBoard();
+    }
+    
+
+    
+    return {players, createShips, shipsArray, boards}
 }
 
-game();
+// game();
 
 function getShips() {
     const shipsArray = [];
@@ -36,3 +50,55 @@ function getShips() {
     shipsArray.push(patrolBoard, sumbarine, destroyer, battleship, carrier);
     return shipsArray;
 }
+
+function GameControllerTemplate(playerOneName = "Player One",playerTwoName = "Player Two") {
+    const board = Gameboard();
+  
+    const players = [
+      {
+        name: playerOneName,
+        token: 1
+      },
+      {
+        name: playerTwoName,
+        token: 2
+      }
+    ];
+  
+    let activePlayer = players[0];
+  
+    const switchPlayerTurn = () => {
+      activePlayer = activePlayer === players[0] ? players[1] : players[0];
+    };
+    const getActivePlayer = () => activePlayer;
+  
+    const printNewRound = () => {
+      board.printBoard();
+      console.log(`${getActivePlayer().name}'s turn.`);
+    };
+  
+    const playRound = (column) => {
+      // Drop a token for the current player
+      console.log(
+        `Dropping ${getActivePlayer().name}'s token into column ${column}...`
+      );
+      board.dropToken(column, getActivePlayer().token);
+  
+      /*  This is where we would check for a winner and handle that logic,
+          such as a win message. */
+  
+      // Switch player turn
+      switchPlayerTurn();
+      printNewRound();
+    };
+  
+    // Initial play game message
+    printNewRound();
+  
+    // For the console version, we will only use playRound, but we will need
+    // getActivePlayer for the UI version, so I'm revealing it now
+    return {
+      playRound,
+      getActivePlayer
+    };
+  }
--- src/index.js
+++ src/index.js
@@ -5,8 +5,65 @@ import './style.css';
 import { shipFactory } from './ship';
 import { gameBoardFactory } from './gameboard';
 import { playerFactory } from './player';
-import { game } from './game';
+import { GameController } from './game';
 
+function screenController() {
+    const game = GameController();
+    const placeShipDiv = document.querySelector(".placement-div");
+    const playerBoardDiv = document.querySelector(".player-board");
+    const computerBoardDiv = document.querySelector(".computer-board");
+    let currentShipDirection = "horizontal";
+    game.createShips();
+    let currentShipToPlace = game.shipsArray[0];
+    const humanBoard = game.boards[0];
+    
+    const switchShips = (currentShip) => {
+        const indexofPrevShip = game.shipsArray.findIndex(ship => ship === currentShip);
+        currentShipToPlace = game.shipsArray[indexofPrevShip + 1];
+    }
+
+    const switchShipDirection = () => {
+        if (currentShipDirection === "horizontal") {
+            currentShipDirection = "vertical";
+        } else {
+            currentShipDirection = "horizontal";
+        }
+    }
+
+    const updateScreen = () => {
+        const computerBoard = game.boards[1];
+        humanBoard.createBoard();
+        computerBoard.createBoard();
+        humanBoard.currentBoard.forEach(row => {
+            const numberOfCoordinates = row.length;
+            for (let i = 0; i < numberOfCoordinates; i++) {
+                const square = document.createElement("div");
+                const squareCoordinates = JSON.stringify(row[i]);
+                square.dataset.coordinate = squareCoordinates;
+                playerBoardDiv.append(square);
+            };
+        });
+    }
+
+    const renderPlacingShips = () =>  {
+        const rotateButton = document.querySelector(".rotate-btn");
+        rotateButton.addEventListener("click", switchShipDirection);
+        const playerBoardSquares = playerBoardDiv.children;
+        Array.from(playerBoardSquares).forEach(square => {
+            const squareCoordinate = JSON.parse(square.dataset.coordinate);
+            square.addEventListener("click", () => {
+                humanBoard.placeShip(currentShipToPlace, squareCoordinate, currentShipDirection);
+                switchShips(currentShipToPlace);
+            });
+        });
+    };
+
+    updateScreen();
+    renderPlacingShips();
+    // game.playRound()
+}
+
+screenController();
 
 export function firstRender() {
     const rotateButton = document.querySelector(".rotate-btn");
@@ -19,17 +76,17 @@ export function firstRender() {
     })
 }
 
-export function renderPlacingShips(playerObject, ships) {
-    const playerBoard = document.querySelector(".player-board");
-    const playerBoardSquares = playerBoard.children;
-    let counter = 0;
-    const currentShip = ships[counter];
-    Array.from(playerBoardSquares).forEach(square => {
-        const squareCoordinate = JSON.parse(square.dataset.coordinate);
-        square.addEventListener("click", () => {playerObject.placeShip(currentShip, squareCoordinate, "horizontal")});
-        counter += 1;
-    })
-}
+// export function renderPlacingShips(playerObject, ships) {
+//     const playerBoard = document.querySelector(".player-board");
+//     const playerBoardSquares = playerBoard.children;
+//     let counter = 0;
+//     const currentShip = ships[counter];
+//     Array.from(playerBoardSquares).forEach(square => {
+//         const squareCoordinate = JSON.parse(square.dataset.coordinate);
+//         square.addEventListener("click", () => {playerObject.placeShip(currentShip, squareCoordinate, "horizontal")});
+//         counter += 1;
+//     })
+// }
 
 export function renderBoard(player, boardData) {
     const humanBoard = document.querySelector(".player-board");
@@ -67,3 +124,52 @@ export function rotateShip() {
     currentChoice = "horizontal";
     return currentChoice;
 }
+
+function Screen() {
+    const game = GameController();
+    const playerTurnDiv = document.querySelector('.turn');
+    const boardDiv = document.querySelector('.board');
+  
+    const updateScreen = () => {
+      // clear the board
+      boardDiv.textContent = "";
+  
+      // get the newest version of the board and player turn
+      const board = game.getBoard();
+      const activePlayer = game.getActivePlayer();
+  
+      // Display player's turn
+      playerTurnDiv.textContent = `${activePlayer.name}'s turn...`
+  
+      // Render board squares
+      board.forEach(row => {
+        row.forEach((cell, index) => {
+          // Anything clickable should be a button!!
+          const cellButton = document.createElement("button");
+          cellButton.classList.add("cell");
+          // Create a data attribute to identify the column
+          // This makes it easier to pass into our `playRound` function 
+          cellButton.dataset.column = index
+          cellButton.textContent = cell.getValue();
+          boardDiv.appendChild(cellButton);
+        })
+      })
+    }
+  
+    // Add event listener for the board
+    function clickHandlerBoard(e) {
+      const selectedColumn = e.target.dataset.column;
+      // Make sure I've clicked a column and not the gaps in between
+      if (!selectedColumn) return;
+      
+      game.playRound(selectedColumn);
+      updateScreen();
+    }
+    boardDiv.addEventListener("click", clickHandlerBoard);
+  
+    // Initial render
+    updateScreen();
+  
+    // We don't need to return anything from this module because everything is encapsulated inside this screen controller.
+  }
+  
