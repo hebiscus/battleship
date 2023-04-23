473ca394fb41bd42d6f36202128ebf67b865d600
hebiscus <107350293+hebiscus@users.noreply.github.com>
2023-04-23 18:02:43 +0200

    clean unneeded

--- src/index.js
+++ src/index.js
@@ -238,17 +238,6 @@ function screenController() {
 
 screenController();
 
-export function firstRender() {
-    const rotateButton = document.querySelector(".rotate-btn");
-    const shipDirection = rotateShip();
-    rotateButton.addEventListener("click", () => {shipDirection});
-    const playerBoard = document.querySelector(".player-board");
-    const playerBoardSquares = playerBoard.children();
-    playerBoardSquares.forEach(square => {
-        square.addEventListener("click", () => {})
-    })
-}
-
 export function renderBoard(player, boardData) {
     const humanBoard = document.querySelector(".player-board");
     const computerBoard = document.querySelector(".computer-board");
@@ -276,51 +265,5 @@ export function renderBoard(player, boardData) {
     };
 };
 
-function Screen() {
-    const game = GameController();
-    const playerTurnDiv = document.querySelector('.turn');
-    const boardDiv = document.querySelector('.board');
-  
-    const updateScreen = () => {
-      // clear the board
-      boardDiv.textContent = "";
-  
-      // get the newest version of the board and player turn
-      const board = game.getBoard();
-      const activePlayer = game.getActivePlayer();
-  
-      // Display player's turn
-      playerTurnDiv.textContent = `${activePlayer.name}'s turn...`
-  
-      // Render board squares
-      board.forEach(row => {
-        row.forEach((cell, index) => {
-          // Anything clickable should be a button!!
-          const cellButton = document.createElement("button");
-          cellButton.classList.add("cell");
-          // Create a data attribute to identify the column
-          // This makes it easier to pass into our `playRound` function 
-          cellButton.dataset.column = index
-          cellButton.textContent = cell.getValue();
-          boardDiv.appendChild(cellButton);
-        })
-      })
-    }
-  
-    // Add event listener for the board
-    function clickHandlerBoard(e) {
-      const selectedColumn = e.target.dataset.column;
-      // Make sure I've clicked a column and not the gaps in between
-      if (!selectedColumn) return;
-      
-      game.playRound(selectedColumn);
-      updateScreen();
-    }
-    boardDiv.addEventListener("click", clickHandlerBoard);
-  
-    // Initial render
-    updateScreen();
-  
-    // We don't need to return anything from this module because everything is encapsulated inside this screen controller.
-  }
+
   
